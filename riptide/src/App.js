a60d34ec050ff1f5c6d9410971387b0d7da098d4
Mrugank Upadhyay <mrugank2@gmail.com>
2020-10-03 18:33:23 -0400

    refactored design

--- riptide/src/App.js
+++ riptide/src/App.js
@@ -38,18 +38,18 @@ class App extends Component {
   render() {
     const menustyle = {
       width: "20%",
-      padding: "15px"
-    }
+      padding: "15px",
+    };
     return (
       <React.Fragment>
         <div>
-          <div className="sidebarStyle" style = {menustyle}>
+          <div className="sidebarStyle" style={menustyle}>
             <Search />
             <ElapsedHours />
             <FloodIntensity />
           </div>
           <div className="infobarStyle">
-          Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom:{" "}
+            Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom:{" "}
             {this.state.zoom}
           </div>
           <div
--- riptide/src/components/elapsedHours.jsx
+++ riptide/src/components/elapsedHours.jsx
@@ -1,53 +1,78 @@
-import React, { Component } from 'react';
-import { Slider } from '@material-ui/core';
-class ElapsedHours extends Component{
+import React, { Component } from "react";
+import { Slider } from "@material-ui/core";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
 
-    state = {
-        elapsed: 0,
-        sliderLabel: "Elapsed Hours"
-    };
-      
-    valuetext(value) {
-        return value;
-      }
+class ElapsedHours extends Component {
+  state = {
+    elapsed: 0,
+    sliderLabel: "Elapsed Hours",
+  };
 
-    handleEvent = (event, value) => {
-        const elapsed = value;
-        this.setState({ elapsed });
-    };
+  valuetext(value) {
+    return value;
+  }
 
-    render() {
-        const marks = [
-            { value: 1 },
-            { value: 2 },
-            { value: 3 },
-            { value: 4 },
-            { value: 5 },
-            { value: 6 },
-            { value: 7 },
-            { value: 8 },
-            { value: 9 },
-            { value: 10 },
-            { value: 11 },
-            { value: 12 },
-            { value: 13 },
-          ];
-        return (
-            <div>
-                <Slider
-                 defaultValue={1}
-                 aria-labelledby="discrete-slider-always"
-                 aria-label={this.sliderLabel}
-                 min={1}
-                 max={13}
-                 scale={(x) => (x <= 6 ? x : (x - 1) * 2)}
-                 valueLabelDisplay="auto"
-                 marks={marks}
-                 onChange={this.handleEvent}
-                />
-            </div>
-        );
-    }
+  handleEvent = (event, value) => {
+    const elapsed = value;
+    this.setState({ elapsed });
+  };
+
+  muiTheme = createMuiTheme({
+    overrides: {
+      MuiSlider: {
+        thumb: {
+          color: "skyblue",
+        },
+        track: {
+          color: "skyblue",
+        },
+        rail: {
+          color: "black",
+        },
+        markLabel: {
+          color: "white",
+        },
+        markLabelActive: {
+          color: "skyblue",
+        },
+      },
+    },
+  });
+
+  render() {
+    const marks = [
+      { value: 1, label: "1" },
+      { value: 2, label: "2" },
+      { value: 3, label: "3" },
+      { value: 4, label: "4" },
+      { value: 5, label: "5" },
+      { value: 6, label: "6" },
+      { value: 7, label: "12" },
+      { value: 8, label: "14" },
+      { value: 9, label: "16" },
+      { value: 10, label: "18" },
+      { value: 11, label: "20" },
+      { value: 12, label: "22" },
+      { value: 13, label: "24" },
+    ];
+    return (
+      <div className="row">
+        <ThemeProvider theme={this.muiTheme}>
+          <Slider
+            defaultValue={1}
+            aria-labelledby="discrete-slider-always"
+            min={1}
+            max={13}
+            scale={(x) => (x <= 6 ? x : (x - 1) * 2)}
+            valueLabelDisplay="off"
+            marks={marks}
+            onChange={this.handleEvent}
+          />
+        </ThemeProvider>
+      </div>
+    );
+  }
 }
 
 export default ElapsedHours;
