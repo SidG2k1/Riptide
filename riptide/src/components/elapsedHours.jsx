7959ed6921c2e3349814bb39cf7d4b09b6f834b3
Abbiram Ramanathan <abbiram.r@gmail.com>
2020-10-03 17:42:50 -0400

    Fixing elapsed hour slider

--- riptide/src/components/elapsedHours.jsx
+++ riptide/src/components/elapsedHours.jsx
@@ -3,23 +3,32 @@ import { Slider } from '@material-ui/core';
 class ElapsedHours extends Component{
 
     state = {
-        
+        elapsed: 0,
+        sliderLabel: "Elapsed Hours"
     };
       
     valuetext(value) {
         return value;
       }
 
+    handleEvent = (event, value) => {
+        const elapsed = value;
+        this.setState({ elapsed });
+    };
+
     render() {
         return (
             <div>
                 <Slider
-                 defaultValue={80}
-                 //getAriaValueText={valuetext}
+                 defaultValue={1}
                  aria-labelledby="discrete-slider-always"
-                 step={10}
-                 //marks={marks}
-                 valueLabelDisplay="off"
+                 aria-label={this.sliderLabel}
+                 min={1}
+                 max={13}
+                 step={1}
+                 scale={(x) => (x <= 6 ? x : (x - 1) * 2)}
+                 valueLabelDisplay="auto"
+                 onChange={this.handleEvent}
                 />
             </div>
         );
