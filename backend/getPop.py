4cb41d009038fe5ae463e0237e75b7e6796734ec
Siddharth Gupta <siddharthg20012@gmail.com>
2020-10-04 18:20:24 -0400

    updated population estimation algorithm

--- backend/getPop.py
+++ backend/getPop.py
@@ -1,24 +1,20 @@
 from main import latLongDist
 
-def getArea(lat, long):
+
+def getArea (lat, long):
     if (long>74.048724):
-        #Staten Island
         return 0
     elif (lat>40.811551):
-        #Bronx
         return 1
     elif (long<73.8455):
-        #Queens
         return 2
     elif (long<74.024313 and long>73.932979) and (lat<40.831364 and lat>40.700704):
-        #Manhatten
         return 3
     else:
-        #Brooklyn
         return 4
 
 def getPopulation(hmap):
-    populationDensity=[3100.5,12704.1,7935.74,26821.4,13656] # pop/km^2
+    populationDensity=[3100.5,12704.1,7935.74,26821.4,13656]
     averageBuildingCosts = [10000, 20000, 30000, 50000, 40000]
     pmap =[]
 
@@ -31,9 +27,9 @@ def getPopulation(hmap):
             if (x==0) and (y==0):
                     point1 = hmap.pointToLatLong(0, 0)
                     point2 = hmap.pointToLatLong(0, 1)
-                    scale = latLongDist(point1[0], point1[1], point2[0], point2[1])
+                    scale= latLongDist(point1[0], point1[1], point2[0], point2[1])
             point = hmap.pointToLatLong(x,y)
-            if hmap.getWater(x,y) < water_threshold:
+            if data[x][y][1] < water_threshold:
                 row.append([0, 0])
                 continue
             area = getArea(point[0],point[1])
